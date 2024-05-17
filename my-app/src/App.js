e019af38308ee9fc9e46518f58d065057370f95c
imran5644 <imi56444@gmail.com>
2024-05-17 15:40:32 +0530

    Add AddUser Form

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import AddUserForm from "./components/AddUserForm";
+
+function App() {
+  return (
+    <div className="App">
+       <AddUserForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/AddUserForm.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+const AddUserForm = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        role: 'user',
+      });
+
+    const handleChange = (e) => {
+        const  {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Add User</h2>
+      <input
+        type="text"
+        name="username"
+        placeholder="Username"
+        value={formData.username}
+        onChange={handleChange}
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={formData.email}
+        onChange={handleChange}
+      />
+      <select name="role" value={formData.role} onChange={handleChange}>
+        <option value="user">User</option>
+        <option value="admin">Admin</option>
+        <option value="admin">Super Admin</option>
+        <option value="admin">Sales Admin</option>
+      </select>
+      <button type="submit">Add User</button>
+    </form>
+  );
+};
+
+export default AddUserForm;
